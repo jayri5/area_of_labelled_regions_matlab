8f727b6f57ed3e14da579c64559bbcf8528433fa
Jayri Bagchi <46220519+jayri5@users.noreply.github.com>
2020-02-24 21:44:50 +0530

    Add files via upload

---
+++ area_of_regions.m
@@ -0,0 +1,24 @@
+clc; clear all; close all;
+
+%Read Original Image
+I=imread('coins.png');
+%imshow(I) ;  
+%I=rgb2gray(I);
+imshow(I) ;  
+%Convert to Binary
+B=im2bw(I);
+                                            
+%Fill the holes
+C=imfill(B,'holes');
+                                                     
+ %Label the image
+[Label,Total]=bwlabel(C,8);
+
+for i=1:Total
+    %Object Number
+    num = i;
+    [row, col] = find(Label==num);
+    %Find Area
+    Obj_area=numel(row);
+    fprintf('area of region:%d is %f\n',num,Obj_area);
+end
